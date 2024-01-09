36300b4ee7ccb6a530b8adb1549842af4903524f
Chris Pikul <chris@workmaple.com>
2024-01-09 15:22:49 -0800

    added platform for MSVC Windows DLL

--- pg_uuidv7.c
+++ pg_uuidv7.c
@@ -7,12 +7,90 @@
 
 #include <time.h>
 
+*
+ * Monkey-patch the clock_gettime function for windows
+ * 
+ * Thanks to https://stackoverflow.com/questions/5404277/porting-clock-gettime-to-windows
+ */
+#ifdef WIN32
+LARGE_INTEGER
+getFILETIMEoffset()
+{
+	SYSTEMTIME s;
+	FILETIME f;
+	LARGE_INTEGER t;
+
+	s.wYear = 1970;
+	s.wMonth = 1;
+	s.wDay = 1;
+	s.wHour = 0;
+	s.wMinute = 0;
+	s.wSecond = 0;
+	s.wMilliseconds = 0;
+	SystemTimeToFileTime(&s, &f);
+	t.QuadPart = f.dwHighDateTime;
+	t.QuadPart <<= 32;
+	t.QuadPart |= f.dwLowDateTime;
+	return (t);
+}
+
+int clock_gettime(int X, struct timeval *tv)
+{
+	LARGE_INTEGER t;
+	FILETIME f;
+	double microseconds;
+	static LARGE_INTEGER offset;
+	static double frequencyToMicroseconds;
+	static int initialized = 0;
+	static BOOL usePerformanceCounter = 0;
+
+	if (!initialized)
+	{
+		LARGE_INTEGER performanceFrequency;
+		initialized = 1;
+		usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+		if (usePerformanceCounter)
+		{
+			QueryPerformanceCounter(&offset);
+			frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
+		}
+		else
+		{
+			offset = getFILETIMEoffset();
+			frequencyToMicroseconds = 10.;
+		}
+	}
+	if (usePerformanceCounter)
+		QueryPerformanceCounter(&t);
+	else
+	{
+		GetSystemTimeAsFileTime(&f);
+		t.QuadPart = f.dwHighDateTime;
+		t.QuadPart <<= 32;
+		t.QuadPart |= f.dwLowDateTime;
+	}
+
+	t.QuadPart -= offset.QuadPart;
+	microseconds = (double)t.QuadPart / frequencyToMicroseconds;
+	t.QuadPart = microseconds;
+	tv->tv_sec = t.QuadPart / 1000000;
+	tv->tv_usec = t.QuadPart % 1000000;
+	return (0);
+}
+#define CLOCK_REALTIME 0
+#endif
+
 /*
  * Number of microseconds between unix and postgres epoch
  */
 #define EPOCH_DIFF_USECS ((POSTGRES_EPOCH_JDATE - UNIX_EPOCH_JDATE) * USECS_PER_DAY)
 
 PG_MODULE_MAGIC;
+
+/* DLL Export if building with MSVC */
+#ifdef WIN32
+PGDLLEXPORT Datum uuid_generate_v7(PG_FUNCTION_ARGS);
+#endif
 
 PG_FUNCTION_INFO_V1(uuid_generate_v7);
 
@@ -27,8 +105,8 @@ Datum uuid_generate_v7(PG_FUNCTION_ARGS)
 	 */
 	if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
 		ereport(ERROR,
-				(errcode(ERRCODE_INTERNAL_ERROR),
-				 errmsg("could not get CLOCK_REALTIME")));
+						(errcode(ERRCODE_INTERNAL_ERROR),
+						 errmsg("could not get CLOCK_REALTIME")));
 
 	tms = ((uint64_t)ts.tv_sec * 1000) + ((uint64_t)ts.tv_nsec / 1000000);
 	tms = pg_hton64(tms << 16);
@@ -36,8 +114,8 @@ Datum uuid_generate_v7(PG_FUNCTION_ARGS)
 
 	if (!pg_strong_random(&uuid->data[6], UUID_LEN - 6))
 		ereport(ERROR,
-				(errcode(ERRCODE_INTERNAL_ERROR),
-				 errmsg("could not generate random values")));
+						(errcode(ERRCODE_INTERNAL_ERROR),
+						 errmsg("could not generate random values")));
 
 	/*
 	 * Set magic numbers for a "version 7" UUID, see
@@ -60,6 +138,11 @@ static uint64_t uuid_v7_to_uint64(pg_uuid_t *uuid)
 	return ts;
 }
 
+/* DLL Export if building with MSVC */
+#ifdef WIN32
+PGDLLEXPORT Datum uuid_v7_to_timestamptz(PG_FUNCTION_ARGS);
+#endif
+
 PG_FUNCTION_INFO_V1(uuid_v7_to_timestamptz);
 
 Datum uuid_v7_to_timestamptz(PG_FUNCTION_ARGS)
@@ -69,6 +152,11 @@ Datum uuid_v7_to_timestamptz(PG_FUNCTION_ARGS)
 
 	PG_RETURN_TIMESTAMPTZ(ts);
 }
+
+/* DLL Export if building with MSVC */
+#ifdef WIN32
+PGDLLEXPORT Datum uuid_v7_to_timestamp(PG_FUNCTION_ARGS);
+#endif
 
 PG_FUNCTION_INFO_V1(uuid_v7_to_timestamp);
 
@@ -93,8 +181,8 @@ static Datum uuid_uint64_to_v7(uint64_t ts, bool zero)
 		memset(&uuid->data[6], 0, UUID_LEN - 6);
 	else if (!pg_strong_random(&uuid->data[6], UUID_LEN - 6))
 		ereport(ERROR,
-				(errcode(ERRCODE_INTERNAL_ERROR),
-				 errmsg("could not generate random values")));
+						(errcode(ERRCODE_INTERNAL_ERROR),
+						 errmsg("could not generate random values")));
 
 	/*
 	 * Set magic numbers for a "version 7" UUID, see
@@ -105,6 +193,11 @@ static Datum uuid_uint64_to_v7(uint64_t ts, bool zero)
 
 	PG_RETURN_UUID_P(uuid);
 }
+
+/* DLL Export if building with MSVC */
+#ifdef WIN32
+PGDLLEXPORT Datum uuid_timestamptz_to_v7(PG_FUNCTION_ARGS);
+#endif
 
 PG_FUNCTION_INFO_V1(uuid_timestamptz_to_v7);
 
@@ -118,6 +211,11 @@ Datum uuid_timestamptz_to_v7(PG_FUNCTION_ARGS)
 
 	return uuid_uint64_to_v7(ts, zero);
 }
+
+/* DLL Export if building with MSVC */
+#ifdef WIN32
+PGDLLEXPORT Datum uuid_timestamp_to_v7(PG_FUNCTION_ARGS);
+#endif
 
 PG_FUNCTION_INFO_V1(uuid_timestamp_to_v7);
 
